9c5c670b73e2019fb006f2348edbc82bf2bf6bac
github-action-benchmark <github@users.noreply.github.com>
2025-10-06 16:01:32 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for e17b9d84ee70fd88ae2b4d4c7e264c3c1d87127c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759752078387,
+  "lastUpdate": 1759766492132,
   "repoUrl": "https://github.com/noir-lang/aztec-contracts",
   "entries": {
     "ACIR Opcodes": [
@@ -44,6 +44,51 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/aztec-contracts/pull/1/commits/a31e697854df5f044669ffae6f61b7dd51034aa1"
         },
         "date": 1759752077877,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "EcdsaKAccount::constructor",
+            "value": 6343,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "EcdsaKAccount::entrypoint",
+            "value": 4127,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "EcdsaKAccount::process_message",
+            "value": 1,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "EcdsaKAccount::sync_private_state",
+            "value": 1,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "EcdsaKAccount::verify_private_authwit",
+            "value": 2222,
+            "unit": "acir_opcodes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "noir-lang",
+            "username": "noir-lang"
+          },
+          "committer": {
+            "name": "noir-lang",
+            "username": "noir-lang"
+          },
+          "id": "e17b9d84ee70fd88ae2b4d4c7e264c3c1d87127c",
+          "message": "benchmark",
+          "timestamp": "2025-10-06T09:03:04Z",
+          "url": "https://github.com/noir-lang/aztec-contracts/pull/1/commits/e17b9d84ee70fd88ae2b4d4c7e264c3c1d87127c"
+        },
+        "date": 1759766491277,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
