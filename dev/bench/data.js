2bfb58e837eb7a6ce226dc5d0522303b61ab315f
github-action-benchmark <github@users.noreply.github.com>
2025-10-06 12:01:18 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for a31e697854df5f044669ffae6f61b7dd51034aa1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759751908224,
+  "lastUpdate": 1759752078387,
   "repoUrl": "https://github.com/noir-lang/aztec-contracts",
   "entries": {
     "ACIR Opcodes": [
@@ -24,6 +24,51 @@ window.BENCHMARK_DATA = {
           {
             "name": "EcdsaKAccount::entrypoint",
             "value": 4127,
+            "unit": "acir_opcodes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "noir-lang",
+            "username": "noir-lang"
+          },
+          "committer": {
+            "name": "noir-lang",
+            "username": "noir-lang"
+          },
+          "id": "a31e697854df5f044669ffae6f61b7dd51034aa1",
+          "message": "benchmark",
+          "timestamp": "2025-10-06T09:03:04Z",
+          "url": "https://github.com/noir-lang/aztec-contracts/pull/1/commits/a31e697854df5f044669ffae6f61b7dd51034aa1"
+        },
+        "date": 1759752077877,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "EcdsaKAccount::constructor",
+            "value": 6343,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "EcdsaKAccount::entrypoint",
+            "value": 4127,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "EcdsaKAccount::process_message",
+            "value": 1,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "EcdsaKAccount::sync_private_state",
+            "value": 1,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "EcdsaKAccount::verify_private_authwit",
+            "value": 2222,
             "unit": "acir_opcodes"
           }
         ]
