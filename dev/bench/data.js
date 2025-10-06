6dfee678493be35ccdc84ca4793dbacfc7890453
github-action-benchmark <github@users.noreply.github.com>
2025-10-06 11:58:28 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for 283d3ba176cf6683188c11d8602c9cd3b2c8ee1f

---
+++ dev/bench/data.js
@@ -0,0 +1,33 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1759751908224,
+  "repoUrl": "https://github.com/noir-lang/aztec-contracts",
+  "entries": {
+    "ACIR Opcodes": [
+      {
+        "commit": {
+          "author": {
+            "name": "noir-lang",
+            "username": "noir-lang"
+          },
+          "committer": {
+            "name": "noir-lang",
+            "username": "noir-lang"
+          },
+          "id": "283d3ba176cf6683188c11d8602c9cd3b2c8ee1f",
+          "message": "benchmark",
+          "timestamp": "2025-10-06T09:03:04Z",
+          "url": "https://github.com/noir-lang/aztec-contracts/pull/1/commits/283d3ba176cf6683188c11d8602c9cd3b2c8ee1f"
+        },
+        "date": 1759751907849,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "EcdsaKAccount::entrypoint",
+            "value": 4127,
+            "unit": "acir_opcodes"
+          }
+        ]
+      }
+    ]
+  }
+}
