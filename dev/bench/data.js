811fe8d6899ab83e9bcfa16f1732d649259ab495
github-action-benchmark <github@users.noreply.github.com>
2025-10-07 13:49:26 +0000

    add Circuit Size (customSmallerIsBetter) benchmark result for 6b11422646f1da50b4c924c6abda194f34777c93

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759844965793,
+  "lastUpdate": 1759844966576,
   "repoUrl": "https://github.com/noir-lang/aztec-contracts",
   "entries": {
     "ACIR Opcodes": [
@@ -606,6 +606,56 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/aztec-contracts/pull/1/commits/3e30ac11d3eafcc141428feb043b58eea39b0b4e"
         },
         "date": 1759840873960,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ecdsa_k_account_contract-constructor",
+            "value": 29742,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "ecdsa_k_account_contract-entrypoint",
+            "value": 55222,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "ecdsa_k_account_contract-verify_private_authwit",
+            "value": 47042,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "private_token_contract-constructor",
+            "value": 23790,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "private_token_contract-mint",
+            "value": 22269,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "private_token_contract-transfer",
+            "value": 105293,
+            "unit": "circuit_size"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "noir-lang",
+            "username": "noir-lang"
+          },
+          "committer": {
+            "name": "noir-lang",
+            "username": "noir-lang"
+          },
+          "id": "6b11422646f1da50b4c924c6abda194f34777c93",
+          "message": "benchmark",
+          "timestamp": "2025-10-06T09:03:04Z",
+          "url": "https://github.com/noir-lang/aztec-contracts/pull/1/commits/6b11422646f1da50b4c924c6abda194f34777c93"
+        },
+        "date": 1759844966562,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
