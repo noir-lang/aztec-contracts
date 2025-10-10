03fd308119c9801b7fbc93ae10496d6c566b3e93
github-action-benchmark <github@users.noreply.github.com>
2025-10-10 12:46:33 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for f3c8b1dd8dfe154517e229db1ef8d94169310b04

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759922756738,
+  "lastUpdate": 1760100393970,
   "repoUrl": "https://github.com/noir-lang/aztec-contracts",
   "entries": {
     "ACIR Opcodes": [
@@ -653,6 +653,135 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/aztec-contracts/commit/b2356b93115a49535da85ea4180f21dac3fcaa0c"
         },
         "date": 1759922755698,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "auth_registry_contract-set_authorized_private",
+            "value": 812,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "ecdsa_k_account_contract-constructor",
+            "value": 6343,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "ecdsa_k_account_contract-entrypoint",
+            "value": 4127,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "ecdsa_k_account_contract-verify_private_authwit",
+            "value": 2222,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "private_token_contract-constructor",
+            "value": 5798,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "private_token_contract-mint",
+            "value": 5727,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "private_token_contract-transfer",
+            "value": 21823,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-_recurse_subtract_balance",
+            "value": 13253,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-burn_private",
+            "value": 31758,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-cancel_authwit",
+            "value": 785,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-finalize_transfer_to_private_from_private",
+            "value": 33127,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-mint_to_private",
+            "value": 5740,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-prepare_private_balance_increase",
+            "value": 5730,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-private_get_decimals",
+            "value": 1229,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-private_get_name",
+            "value": 1228,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-private_get_symbol",
+            "value": 1228,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-transfer",
+            "value": 6260,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-transfer_in_private",
+            "value": 38309,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-transfer_to_private",
+            "value": 5740,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-transfer_to_public",
+            "value": 31762,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "token_contract-transfer_to_public_and_prepare_private_balance_increase",
+            "value": 37534,
+            "unit": "acir_opcodes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26174818+jialinli98@users.noreply.github.com",
+            "name": "Jialin Li",
+            "username": "jialinli98"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3c8b1dd8dfe154517e229db1ef8d94169310b04",
+          "message": "chore: golden tests for nargo compile (#5)\n\nCo-authored-by: Tom French <15848336+TomAFrench@users.noreply.github.com>",
+          "timestamp": "2025-10-10T13:45:23+01:00",
+          "tree_id": "7ef141fc75dd2e3b0e60ce9f3f7472cdb9a8df30",
+          "url": "https://github.com/noir-lang/aztec-contracts/commit/f3c8b1dd8dfe154517e229db1ef8d94169310b04"
+        },
+        "date": 1760100393541,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
